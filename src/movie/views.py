9245999931e970b3ea8496bd844fce98ccfd97fb
tranggithub <20520323@gm.uit.edu.vn>
2022-11-15 04:35:27 +0700

    Add function sign up

--- src/movie/views.py
+++ src/movie/views.py
@@ -33,8 +33,24 @@ def Loading_Logo(request):
   return HttpResponse(template.render())
 
 def SignUp(request):
-  template = loader.get_template('.\SignUp_LogIn\SignUpFilm.html')
-  return HttpResponse(template.render())
+  if request.method == "POST":
+    usernm = request.POST.get('username')
+    mail = request.POST.get('email')
+    passwd1 = request.POST.get('password1')
+    passwd2 = request.POST.get('password2')
+    if passwd1 == passwd2:
+      nm = request.POST.get('name')
+      myuser = User.objects.create_user(username=usernm,email=mail,password=passwd1)
+      myuser.first_name = nm
+      myuser.save()
+      messages.success(request,"Your account has been created successfully")
+      return redirect('/movies/log_in')
+    else:
+      messages.error(request,"Your confirm password and yor password not the same")
+      return redirect('/movies/sign_up')
+  return render(request,".\SignUp_LogIn\SignUpFilm.html")
+  # template = loader.get_template('.\SignUp_LogIn\SignUpFilm.html')
+  # return HttpResponse(template.render())
 
 def LogIn(request):
   if request.user.is_authenticated:
