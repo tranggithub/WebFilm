97f867e42bad8ae006677c4d254babbffbea1560
tranggithub <20520323@gm.uit.edu.vn>
2022-11-27 05:42:27 +0700

    Add function comment

--- src/movie/views.py
+++ src/movie/views.py
@@ -20,7 +20,8 @@ from django.contrib.auth.forms import PasswordResetForm
 from django.template.loader import render_to_string
 from django.db.models.query_utils import Q
 from django.core.mail import send_mail, BadHeaderError
-
+from .models import *
+from django.urls import reverse_lazy
 class MovieList (ListView):
     model = Movie
     
@@ -152,11 +153,31 @@ def Movies(request):
   return render(request,"Movies/movies.html",{'avatar':ava, 'movies': movies, 'upcoming':upcoming, 'tv_series':tv_series, 'ps':ps})
 
 def WatchFilm(request, movie_id):
-  ava = request.user.profile.avatar.url
-  user = request.user
-  movies = Movie.objects.filter(id=movie_id)
-  episode = movies.get().movie_episode.all()
-  return render(request,".\Trailer_Detail\Watch.html",{'avatar':ava, 'movies': movies, 'episode': episode, 'user':user})
+  if request.user.is_authenticated:
+    movie = Movie.objects.get(pk=movie_id)
+    if request.method == "POST":
+      usr = request.user
+      comment = request.POST.get('comment')
+      try:
+        cmt = Comment.objects.create(movie=movie,user=usr,body=comment)
+        cmt.save()
+      except:
+        messages.error(request,"Fail to comment")
+        url = '/movies/watch/'+ movie_id
+        return redirect(url)
+      messages.success(request,"Comment successfully")
+      url = '/movies/watch/'+ movie_id
+      return redirect(url)
+    ava = request.user.profile.avatar.url
+    movies = Movie.objects.filter(id=movie_id)
+    user = request.user
+    episode = movies.get().movie_episode.all()
+    return render(request,".\Trailer_Detail\Watch.html",{'avatar':ava, 'movies': movies, 'episode': episode, 'user':user})
+  else:
+    messages.error(request,"Please log in!")
+    url=reverse_lazy('log_in')
+    redirect(url)
+
 
 def Detail(request, movie_id):
   ava = request.user.profile.avatar.url
