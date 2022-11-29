3272bf6541f80ba9ada8f511ee5e41747e7193fb
mixity29 <phanthituyetlan471@gmail.com>
2022-11-29 23:08:19 +0700

    error 404

--- src/imdb/settings.py
+++ src/imdb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gq@0#zav+4q9z3r#ru#ujd!s16#n^n!sr^fwnybrnzkhmbvseo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
